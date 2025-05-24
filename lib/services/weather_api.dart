1e648f67c1720413f207eeb4213a6231acdd22b7
Abdirahman  Ali <143823228+AbdirahmanSr@users.noreply.github.com>
2025-05-23 21:04:34 -0600

    first part of weather api code push

---
+++ lib/services/weather_api.dart
@@ -0,0 +1,6 @@
+import 'dart:convert';
+import 'package:http/http.dart' as http;
+
+const String weatherApiKey = '7899c6c8431ca0d4580120ad174ea1ba';
+const String currentWeatherEndPoint = 'https://api.openweathermap.org/data/2.5/weather';
+
